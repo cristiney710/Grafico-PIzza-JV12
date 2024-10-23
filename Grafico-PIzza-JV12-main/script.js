578d11b77460736ba129a6f1ae2b7aac11fd593d
cristiney710 <128044122+cristiney710@users.noreply.github.com>
2024-10-23 08:51:32 -0300

    Add files via upload

---
+++ Grafico-PIzza-JV12-main/script.js
@@ -0,0 +1,25 @@
+const ctx = document.getElementById("myChart").getContext("2dc")
+const myChart = new Chart (ctx,{
+      type: "pie",
+      date: {
+          labels:[
+            "Instagram",
+     "WhatsApp",
+     "Facebook",
+     "WeChat",
+     "TikTok",
+     "DouYin",
+     "Twitter",
+     "Telegram",
+     "FB Messenger",
+     "LINE"
+          ]
+      }
+
+
+
+}
+
+
+)
+
